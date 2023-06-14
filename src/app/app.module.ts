4082af88629de5810c4a24858c9af58165dd06c4
Eduardo0409 <100179167+Eduardo0409@users.noreply.github.com>
2023-06-14 10:35:08 -0300

    Adicionamento do Service timer e exclusao das rotas mult-table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { RootComponent } from './root/root.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { MultTableComponent } from './mult-table/mult-table.component';
-import { MultTableDetalsComponent } from './mult-table-detals/mult-table-detals.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { RouterModule } from '@angular/router';
@@ -16,15 +14,11 @@ import { RouterModule } from '@angular/router';
     RouterModule.forRoot([
       {path: '', component: HomeComponent},
       {path: 'about', component: AboutComponent},
-      {path: 'mult-table', component: MultTableComponent},
-      {path: 'mult-table/:n', component: MultTableDetalsComponent},
     ]),
   ],
   declarations: [
     RootComponent, 
     NavbarComponent, 
-    MultTableComponent, 
-    MultTableDetalsComponent, 
     HomeComponent, 
     AboutComponent,
   ],
